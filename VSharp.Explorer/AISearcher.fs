9c58eda3fc1151df775a7066961bdedb6356fce2
Parzival-05 <parzivalwasd@gmail.com>
2025-08-12 14:52:25 +0300

    Revert "Rename PathCondition according to client's naming"
    This reverts commit e9cb37576b8c29625f79082b3da87287197e105f.

--- VSharp.Explorer/AISearcher.fs
+++ VSharp.Explorer/AISearcher.fs
@@ -180,7 +180,7 @@ type internal AISearcher(oracle: Oracle, aiAgentTrainingMode: Option<AIAgentTrai
                     State(
                         s.Id,
                         s.Position,
-                        s.PathConditionSize,
+                        s.PathCondition,
                         s.VisitedAgainVertices,
                         s.VisitedNotCoveredVerticesInZone,
                         s.VisitedNotCoveredVerticesOutOfZone,
@@ -383,7 +383,7 @@ type internal AISearcher(oracle: Oracle, aiAgentTrainingMode: Option<AIAgentTrai
                             firstFreePositionInParentsOf <- firstFreePositionInParentsOf + state.Children.Length
 
                             index_pcToState.[firstFreePositionInPcToState] <-
-                                int64 pathConditionVerticesIds[state.PathConditionSize.Id]
+                                int64 pathConditionVerticesIds[state.PathCondition.Id]
 
                             index_pcToState.[firstFreePositionInPcToState + gameState.States.Length] <-
                                 int64 stateIds[state.Id]
--- VSharp.ML.GameServer/Messages.fs
+++ VSharp.ML.GameServer/Messages.fs
@@ -153,7 +153,7 @@ type StateHistoryElem =
 type State =
     val Id: uint<stateId>
     val Position: uint<byte_offset> // to basic block id
-    val PathConditionSize: PathConditionVertex
+    val PathCondition: PathConditionVertex
     val VisitedAgainVertices: uint
     val VisitedNotCoveredVerticesInZone: uint
     val VisitedNotCoveredVerticesOutOfZone: uint
@@ -177,7 +177,7 @@ type State =
         ) =
         { Id = id
           Position = position
-          PathConditionSize = pathCondition
+          PathCondition = pathCondition
           VisitedAgainVertices = visitedAgainVertices
           VisitedNotCoveredVerticesInZone = visitedNotCoveredVerticesInZone
           VisitedNotCoveredVerticesOutOfZone = visitedNotCoveredVerticesOutOfZone
